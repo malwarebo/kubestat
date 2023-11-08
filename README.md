8dcd06d9b8f822d7304ddff91b9868764de5790b
Irfan <irfnhm@gmail.com>
2023-11-08 13:04:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# kubestat
+
+Simple k8s status cli tool
+
+Clone the repository
+
+Run locally within the package scope
+
+```bash
+./kubestat
+```
+
+or
+
+```bash
+cd kubestat
+sudo cp kubestat /usr/local/bin
+```
+
+Run the command  `kubestat` from anywhere in the CLI.
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/olekukonko/tablewriter"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func getKubeClient() (*kubernetes.Clientset, error) {
+	kubeconfigPath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientset, nil
+}
+
+func displayKubernetesStatus() {
+	clientset, err := getKubeClient()
+	if err != nil {
+		fmt.Printf("Error creating Kubernetes client: %v\n", err)
+		return
+	}
+
+	pods, err := clientset.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("Error listing pods: %v\n", err)
+		return
+	}
+
+	// Create a new table
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"ID", "NAME", "STATUS"})
+
+	for _, pod := range pods.Items {
+		id := string(pod.ObjectMeta.UID)
+		name := pod.ObjectMeta.Name
+		status := strings.ToLower(string(pod.Status.Phase))
+
+		// Add a row to the table
+		table.Append([]string{id, name, status})
+	}
+
+	// Render the table
+	table.Render()
+}
+
+func main() {
+	flag.Parse()
+
+	fmt.Println("Checking Kubernetes Status:")
+	displayKubernetesStatus()
+}
